d2c4199af4a007789577d6827824abcdda47c921
amalsanjayy18-oss <amalsanjayy18@gmail.com>
2026-06-21 01:54:23 -0700

    greater or lesser modification

--- new.py
+++ new.py
@@ -2,3 +2,11 @@ x=int(input("Enter a number "))
 y=int(input("Enter another number "))
 z=x+y
 print("The sum is",z)
+
+if z>10:
+    print("the sum is greater than 10")
+elif z==10:
+    print("the sum is equal to 10")
+elif z<10:
+    print("the sum is less than 10")
+    
